fb4128b28611198485c5ebe3a6ca7c5bb6b8be22
Davey <36044@ma-web.nl>
2023-03-24 09:50:21 +0100

    opdrachten

---
+++ FunctionOpdracht/FunctionOpdracht4/FunctionOpdracht4/Program.cs
@@ -0,0 +1,28 @@
+using System;
+
+namespace FunctionOpdracht4
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Program program= new Program();
+
+
+            int result = program.AddNumbers(2, 6, 18);
+
+            Console.WriteLine("should be 26?");
+            Console.WriteLine(result);
+        }
+
+
+        private int AddNumbers(int val1 , int val2 , int val3 ) 
+        {
+            int awnser = val1 + val2 + val3;
+            return awnser;
+        }
+        //1) maak hier een function AddNumbers die 3 argumenten heeft
+        //2) de function telt de 3 argumenten bij elkaar op en geeft dat als resultaat terug
+
+    }
+}
