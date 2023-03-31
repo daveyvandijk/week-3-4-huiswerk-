41b88f3b4bee54b7823a70c84d87ab99c6c6f43e
Davey <36044@ma-web.nl>
2023-03-31 09:40:52 +0200

    opdrachten

---
+++ CalculateOpdracht/CalculateOpdracht2/CalculateOpdracht2/Program.cs
@@ -0,0 +1,60 @@
+﻿using System;
+
+namespace CalculateOpdracht2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+
+            //we gaan nu dingen kopen
+
+            int controllersInMandje = 2;
+
+
+            //koop 2 controllers
+            
+            
+
+
+
+            //hij is in voorraad snel kopen!
+            int ps5 = 1;
+
+            //koop 1 ps5
+            
+
+            //koop de games:
+
+            int eldenRing = 1;
+
+            int shantae = 1;
+
+            int spongebob = 1;
+                spongebob--;
+
+
+            //hmmm weten we het zeker?
+            //haal 1 game weg met -- of -= 1
+
+            //nu nog de prijs
+
+
+            double ps5Prijs = 499.99;
+            double controllerPrijs = 69.99;
+            double gamePrijs = 59.99;
+
+
+            double totaalControllerPrijs = controllerPrijs * controllersInMandje;// controllersInMandje keer controllerPrijs
+            double totaalgamePrijs = eldenRing * gamePrijs + shantae * gamePrijs + spongebob * gamePrijs;// game1 keer gamePrijs  + game2 keer gamePrijs (let op je haakjes!)
+            double totaalps5prijs = ps5 * ps5Prijs;
+
+
+            double totaalPrijs = totaalControllerPrijs + totaalgamePrijs + totaalps5prijs;// tel hier alles bij elkaar op
+
+            Console.WriteLine(totaalPrijs);
+            //er moet nu 759.95 staan
+        }
+    }
+}
---
+++ VariableOpdracht/VariableOpdracht2/VariableOpdracht2/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.IO;
+
+namespace VariableOpdracht2
+{
+    internal class Program
+    {
+        //class variables & gebruik
+        static void Main(string[] args)
+        {
+
+            //ga naar de file TwitchStream.cs in je solution explorer in visual studio
+            TwitchStream twitchStream = new TwitchStream();
+
+
+
+            //geef nu de stream de volgende waardes:
+            //name, waarde: je favoriete streamer
+            //lastStreamName, waarde: de streamname van de laatste keer
+            //streamGame, waarde een nieuwe instance van Game (new Game("naam van de game hier"})
+            //followers, waarde: hoeveel followers
+            //automod, waarde: staat automod aan?
+            twitchStream.name = "SpaceBoy";
+            twitchStream.lastStreamName = "Gta Rolplay James Randell";
+            twitchStream.streamGame = (new Game ( "Gta 5" ));
+            twitchStream.followers = 10000000;
+            twitchStream.automod = true;
+
+
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
